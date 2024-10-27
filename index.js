f0ab1e6689c0d08f81ca90d6db5c6b8929a6934f
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-10-27 13:16:55 +1000

    Small change to query string of test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // console.log('I\'m working!')
-fetch('https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&mode=analogic&count=6')
+fetch('https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=6')
     .then(resp => resp.json())
     .then(data => {
         console.log(data.colors)
