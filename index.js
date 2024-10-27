c3b8c2f86015f4a4bddaa232fbb2688fd4936b17
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-10-27 13:14:45 +1000

    Test of getting hex color values from 200 reponse
    This gets the color in hex values and console logs them.
    
    Just a test but worth having a record of

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // console.log('I\'m working!')
+fetch('https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&mode=analogic&count=6')
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data.colors)
+        for (color of data.colors){
+            console.log(color.hex.value)
+        }
+    })
