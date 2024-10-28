99a82c42e2086e3f90f064e5811e58047b3e5633
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-10-28 16:08:21 +1000

    Fetching data from form

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ document.addEventListener('submit', (e) => {
 const handleSubmit = (e) => {
     e.preventDefault();
     const data = new FormData(colorForm);
+    const selectedColor = data.get('color').slice(1)
+    const mode = data.get('scheme-mode')
     console.log(data.get('color'))
+    console.log(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${mode}&count=6`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${mode}&count=6`)
+    .then(resp => resp.json())
+    .then(scheme => {
+        console.log(scheme)
+        for (color of scheme.colors){
+            console.log(color.hex.value)
+        }
+    })
 }
 
 // ?color-picker=%23e9afaf&scheme-mode=monochrome-light
 
-fetch('https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=6')
-    .then(resp => resp.json())
-    .then(data => {
-        console.log(data.colors)
-        for (color of data.colors){
-            console.log(color.hex.value)
-        }
-    })
