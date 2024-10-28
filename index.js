5f3185f47ab5824a89461a67ff82450007838bf5
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-10-28 15:11:53 +1000

    Added event listener
    Added event listener and listening for submit and creating new const formData

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 // console.log('I\'m working!')
+const colorForm = document.getElementById('color-form');
+
+document.addEventListener('submit', (e) => {
+    handleSubmit(e);
+})
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(colorForm);
+    console.log(data.get('color'))
+}
+
+// ?color-picker=%23e9afaf&scheme-mode=monochrome-light
+
 fetch('https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=6')
     .then(resp => resp.json())
     .then(data => {
