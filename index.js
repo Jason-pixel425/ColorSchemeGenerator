f0b270e8af83f5cdcc2bda3dec35c73b2040059c
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-10-28 17:53:55 +1000

    Working
    Got the scheme rendering

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// console.log('I\'m working!')
 const colorForm = document.getElementById('color-form');
 
 document.addEventListener('submit', (e) => {
@@ -10,17 +9,21 @@ const handleSubmit = (e) => {
     const data = new FormData(colorForm);
     const selectedColor = data.get('color').slice(1)
     const mode = data.get('scheme-mode')
-    console.log(data.get('color'))
-    console.log(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${mode}&count=6`)
-    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${mode}&count=6`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${mode}&count=5`)
     .then(resp => resp.json())
     .then(scheme => {
-        console.log(scheme)
-        for (color of scheme.colors){
-            console.log(color.hex.value)
-        }
+        renderScheme(scheme)
     })
 }
 
-// ?color-picker=%23e9afaf&scheme-mode=monochrome-light
 
+ const renderScheme = (scheme) => {
+    let renderHTML = ''
+    let rowTwo = ''
+    for (color of scheme.colors){
+        renderHTML += `<div class="scheme" style="background-color:${color.hex.value}"></div>`
+        rowTwo += `<p>${color.hex.value}</p>`
+    }
+    renderHTML += rowTwo
+    document.getElementById('generated-scheme').innerHTML = renderHTML;
+ }
